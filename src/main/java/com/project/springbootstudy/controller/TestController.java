bd46315c2414ffbbff094e3658823348c833cfae
김나영(Nana) <trainee204@dxschool.co.kr>
2024-08-29 13:05:57 +0900

    fix: add description about port number

--- src/main/java/com/project/springbootstudy/controller/TestController.java
+++ src/main/java/com/project/springbootstudy/controller/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ResponseBody //이 클래스는 응답을 줄때 page가 아니라 입력값 그대로 보내줄거야
 public class TestController {
 
-    @GetMapping("/{name}/{message}") //요청 예시 : ip주소/김나영/스윽 몰래 들어와본다
+    @GetMapping("/{name}/{message}") //요청 예시 : ip주소:port번호/김나영/스윽 몰래 들어와본다
     public String helloMan(@PathVariable(name="name") String name , @PathVariable(name="message") String message) {
 
         System.out.println(name + "님이 접속했습니다. 메세지 : " + message);
